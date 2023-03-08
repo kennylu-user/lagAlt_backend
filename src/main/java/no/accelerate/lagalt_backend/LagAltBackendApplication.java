e66722857ecaea2d3b73251eb1ddb67271e69bdc
Kenny Lu <kenny.lu@no.experis.com>
2023-03-08 11:12:27 +0100

    Initial commit

---
+++ src/main/java/no/accelerate/lagalt_backend/LagAltBackendApplication.java
@@ -0,0 +1,13 @@
+package no.accelerate.lagalt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagAltBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LagAltBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/accelerate/lagalt_backend/LagAltBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.accelerate.lagalt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagAltBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
