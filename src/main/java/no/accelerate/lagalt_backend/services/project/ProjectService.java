3fa0489da59d6424c34212f343ceae5e3671ff89
Kenny Lu <kenny.lu@no.experis.com>
2023-03-13 14:53:41 +0100

    Added img_url field and project controller extended

--- src/main/java/no/accelerate/lagalt_backend/services/project/ProjectService.java
+++ src/main/java/no/accelerate/lagalt_backend/services/project/ProjectService.java
@@ -1,5 +1,7 @@
 package no.accelerate.lagalt_backend.services.project;
 
+import no.accelerate.lagalt_backend.models.Application;
+import no.accelerate.lagalt_backend.models.User;
 import no.accelerate.lagalt_backend.models.Project;
 import no.accelerate.lagalt_backend.services.CrudService;
 
@@ -7,4 +9,7 @@ import java.util.Set;
 
 
 public interface ProjectService extends CrudService<Project, Integer> {
+    public Set<Application> findAllProjectApplications(int id);
+    public Set<User> findAllMembers(int id);
+
 }
